65172802a33fa64f352ce213410c140c54164d2d
sassiek <68421234+sassiek@users.noreply.github.com>
2021-06-23 16:29:05 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# gulp-sass-ts
+
+Example gulp setup with SCSS and TypeScript.
+
+Configured to watch for changes in `app/js` and `app/scss`
+
+### Instructions for use
+- clone repo
+- run `npm install`
+- run `./node_modules/gulp/bin/gulp.js watch`
---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+var ts = require('gulp-typescript');
+var tsProject = ts.createProject('tsconfig.json');
+
+function tsCompile(cb) {
+    return tsProject.src()
+        .pipe(tsProject())
+        .js.pipe(gulp.dest("app/js/dist"));
+    cb();
+}
+
+function sassCompile(cb) {
+    return gulp.src("app/scss/style.scss")
+        .pipe(sourcemaps.init())
+        .pipe(sass())
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest("app/css"));
+    cb();
+}
+
+function watch() {
+    sassCompile()
+    tsCompile()
+    gulp.watch("app/scss/**/*.scss", sassCompile);
+    gulp.watch("app/js/*.ts", tsCompile);
+}
+
+exports.sass = sassCompile;
+exports.ts = tsCompile;
+exports.watch = watch;
